09a5daef516595e860ff5a8cc71caec68ad812b6
Per-Åke Minborg <minborg@speedment.com>
2015-11-12 13:52:36 -0800

    Add insert and update in database

---
+++ src/main/java/com/blogspot/minborgsjavapot/insert_in_db/Main.java
@@ -0,0 +1,58 @@
+package com.blogspot.minborgsjavapot.insert_in_db;
+
+import com.company.speedment.test.javapot.JavapotApplication;
+import com.company.speedment.test.javapot.db0.javapot.country.Country;
+import com.speedment.Manager;
+import com.speedment.Speedment;
+import com.speedment.exception.SpeedmentException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Setup
+        Speedment speedment = new JavapotApplication().withPassword("javapot").build();
+        Manager<Country> countries = speedment.managerOf(Country.class);
+
+        try {
+            countries.newInstance()
+                    .setName("United States")
+                    .setLocalName("United States")
+                    .setCode(1)
+                    .setDomain(".us")
+                    .persist();
+
+            countries.newInstance()
+                    .setName("Germany")
+                    .setLocalName("Deutschland")
+                    .setCode(49)
+                    .setDomain(".de")
+                    .persist();
+
+            // Needless to say, you can call the 
+            // setters in any order.
+            countries.newInstance()
+                    .setDomain(".uk")
+                    .setCode(44)
+                    .setName("United Kingdom")
+                    .setLocalName("United Kingdom")
+                    .setDomain(".uk")
+                    .persist();
+
+            countries.newInstance()
+                    .setName("Sweden")
+                    .setLocalName("Sverige")
+                    .setCode(40) // Intentionally wrong
+                    .setDomain(".se")
+                    .persist();
+
+            countries.stream()
+                    .filter(Country.NAME.equal("Sweden"))
+                    .findAny()
+                    .ifPresent(c -> c.setCode(46).update());
+
+        } catch (SpeedmentException se) {
+
+        }
+    }
+
+}
